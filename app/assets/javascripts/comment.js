bf12c24071bcddf4b9fb841c54ad2d959ce426a3
jrobins <jessrrobins@gmail.com>
2016-03-27 19:55:45 +0000

    comments are looking better

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,14 +1,34 @@
 $(document).ready(function () {
       
     $(document).on('click','.comment-card-clickable', function(){
-        $(this).find('.resolve-btn').show();
+        var activeElem = $('.comment-card-active');
+        if (activeElem.length > 0) {
+            removeActive(activeElem)
+        }
         $.ajax({
             url: $(this).attr('ajax_path'),  
             data: { },
             dataType: 'script'
           });
+        $(this).find('.resolve-btn').show();
         return false;
-        
+    });
+    
+    $(document).on('click','.comment-card-active', function(e){
+        e.stopPropagation();
+    });
+    
+    $(document).on('click','.modal', function(){
+        var elem = $(this).find('.comment-card-active');
+        removeActive(elem);
     });
     
 });
+
+function removeActive(elem){
+    elem.addClass('comment-card-clickable');
+    elem.removeClass('comment-card-active');
+    elem.find('.comment-form').remove();
+    elem.find('.comment-btns').show();
+    elem.find('.resolve-btn').hide();
+}
