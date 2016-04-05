23b2ad161348b92ab6cee3196dfe9f7c39616919
jrobins <jessrrobins@gmail.com>
2016-04-05 01:46:10 +0000

    scrollspy slow as crap

--- app/assets/javascripts/materialize_stuff.js
+++ app/assets/javascripts/materialize_stuff.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $(".dropdown-button").dropdown({ hover: false });
     $('select').material_select();
-    $('.scrollspy').scrollSpy();
     $('.bookmarks .pinned').pushpin();
 });
