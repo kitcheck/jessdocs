b19180a3b9d9a9434718e08ef8a049825cf7fb5c
jrobins <jessrrobins@gmail.com>
2016-04-07 02:46:33 +0000

    holy shit drag and drop is working. just the js stuff

--- app/assets/javascripts/materialize_stuff.js
+++ app/assets/javascripts/materialize_stuff.js
@@ -2,4 +2,6 @@ $(document).ready(function () {
     $(".dropdown-button").dropdown({ hover: false });
     $('select').material_select();
     $('.bookmarks .pinned').pushpin();
+    
+    $("ul.sortable").sortable();
 });
